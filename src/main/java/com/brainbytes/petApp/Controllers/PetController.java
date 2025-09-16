d20ce8146f4142f802bd7a4d50b4d189070be71f
Haytam ELGBOURI <130654725+Haytam-Nagi@users.noreply.github.com>
2025-09-16 17:58:58 +0100

    completion of pet controller

--- src/main/java/com/brainbytes/petApp/Controllers/PetController.java
+++ src/main/java/com/brainbytes/petApp/Controllers/PetController.java
@@ -28,5 +28,29 @@ public class PetController {
         return petRepository.findAll();
     }
 
+    @GetMapping(path = "/pet/{id}")
+    public Pet GetPetById(@PathVariable Long id){
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping(path = "/pet/{id}")
+    public Pet UpdatePetById(@PathVariable Long id, @RequestBody Pet petDetails){
+        return petRepository.findById(id).map(pet ->{
+            pet.setAge(petDetails.getAge());
+            pet.setColor(petDetails.getColor());
+            pet.setGender(petDetails.getGender());
+            pet.setWeight(petDetails.getWeight());
+            pet.setVaccinationStatus(petDetails.getVaccinationStatus());
+            pet.setNote(petDetails.getNote());
+            pet.setAllergy(petDetails.getAllergy());
+            pet.setMedicalHistory(petDetails.getMedicalHistory());
+            return petRepository.save(pet);
+        }).orElse(null);
+    }
+
+    @DeleteMapping(path = "/pet/{id}")
+    public void DeletePetById(@PathVariable Long id){
+        petRepository.deleteById(id);
+    }
 
 }
